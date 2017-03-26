69692b702d5eeada358df346192d5d229dddc618
Obada Kadri <brobada@gmail.com>
2017-03-26 13:35:07 -0700

    Add config.js to repo

---
+++ config.js
@@ -0,0 +1,10 @@
+// Google API Client ID
+var GAPI_CLIENT_ID = '273724384440-85de3san0p08tod1gn9lnlicropdhqau.apps.googleusercontent.com';
+
+// Google Maps API Key
+var GOOGLE_MAP_KEY = "AIzaSyCoypDyjlv3tAiQwk_Pq5y8eYI6zoQcC5g";
+
+
+// Spread sheet ID
+// TODO: Move to URL
+var SS_ID = '1lcXvxSW6BH3OIa2leTuxP_M1XKxzRnPu2I2f645BsxE';
