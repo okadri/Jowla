2780752bcb960657f6b7f41ec85b0b55bc4017ba
Obada Kadri <brobada@gmail.com>
2017-04-12 21:23:33 -0700

    #15 Confirm before deleting a person

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -28,3 +28,69 @@ app.directive('spinner', [function () {
         }
     };
 }]);
+
+app.directive('confirmButton', ['$document', '$parse', function ($document, $parse) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var buttonId, html, message, nope, title, yep;
+
+            buttonId = Math.floor(Math.random() * 10000000000);
+
+            attrs.buttonId = buttonId;
+
+            var message = attrs.message || "Are you sure?";
+            var yep = attrs.yes || "Yes";
+            var nope = attrs.no || "No";
+            var title = attrs.title || "Confirm";
+
+            html = "<div id=\"button-" + buttonId + "\">\
+            <span class=\"confirmbutton-msg\">" + message + "</span><br>\
+            <button class=\"confirmbutton-yes btn btn-danger\">" + yep + "</button>\
+            <button class=\"confirmbutton-no btn\">" + nope + "</button>\n</div>";
+
+            element.popover({
+                content: html,
+                html: true,
+                trigger: "manual",
+                title: title,
+                placement: 'top'
+            });
+
+            element.bind('click', function (e) {
+                e.stopPropagation();
+                var dontBubble = true;
+
+                element.popover('show');
+                var pop = $("#button-" + buttonId);
+
+                pop.closest(".popover").click(function (e) {
+                    if (dontBubble) {
+                        e.stopPropagation();
+                    }
+                });
+
+                pop.find('.confirmbutton-yes').click(function (e) {
+                    dontBubble = false;
+
+                    var func = $parse(attrs.confirmButton);
+                    func(scope);
+                });
+
+                pop.find('.confirmbutton-no').click(function (e) {
+                    dontBubble = false;
+
+                    $document.off('click.confirmbutton.' + buttonId);
+
+                    element.popover('hide');
+                });
+
+                $document.on('click.confirmbutton.' + buttonId, ":not(.popover, .popover *)", function () {
+                    $document.off('click.confirmbutton.' + buttonId);
+                    element.popover('hide');
+                });
+
+            });
+        }
+    };
+}]);
