99558021d91ef9288c3184eec2b4d5bbc1945457
Obada Kadri <brobada@gmail.com>
2017-04-29 12:38:34 -0700

    Limits filter countries to those in the list

--- js/controllers/FiltersCtrl.js
+++ js/controllers/FiltersCtrl.js
@@ -1,8 +1,25 @@
 app.controller('FiltersCtrl', ['$scope', '$uibModalInstance', 'state', 'actionCreators',
     function ($scope, $uibModalInstance, state, actionCreators) {
+
+        // Construct a sorted unique country list from people list
+        var userCountries = state.people.ids.filter(function (id) {
+            return state.people.list[id].country;
+        }).map(function (id) {
+            return state.people.list[id].country;
+        }).reduce(function (result, current) {
+            if (result.indexOf(current) < 0) {
+                result.push(current);
+                return result;
+            } else {
+                return result;
+            }
+        }, []).sort(function(c1,c2) {
+            return (c1.name > c2.name) ? 1 : (c1.name < c2.name) ? -1 : 0;
+        });
+
         $scope.view = {
             state: state,
-            countries: countries,
+            userCountries: userCountries,
         };
 
         $scope.apply = function () {
